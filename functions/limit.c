058638abaddcc20a70991a0a3a284a408909c81a
Keizou Wang <keizouw8@gmail.com>
2023-11-18 11:28:57 -0800

    limit accounts for dx shift with derivative

--- functions/limit.c
+++ functions/limit.c
@@ -1,14 +1,17 @@
+#include "derivative.c"
 #include <math.h>
 
 double side_limit(int direction, double x, double (*f)(double)){
     if(!(direction == -1 || direction == 1)) return NAN;
-    return f(x + direction * 1e-10);
+    double dx = 1e-10;
+    double off_x = x + direction * dx;
+    return f(off_x) - (direction * dx * derivative(off_x, f));
 }
 
 double limit(double x, double (*f)(double)){
     double left = side_limit(-1, x, f);
     double right = side_limit(1, x, f);
 
-    if(fabs(left - right) > 1e-4) return NAN;
+    if(fabs(left - right) > 1e-5) return NAN;
     return (left + right)/2;
 }
